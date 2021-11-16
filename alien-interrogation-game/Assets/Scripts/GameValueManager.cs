06c1c3bef69a748779818a2a024b7d3356eabcf7
Logan Mikulski <lmikulsk@uci.edu>
2021-11-15 17:49:38 -0800

    added lists to track player knowledge in GameValueManager

--- alien-interrogation-game/Assets/Scripts/GameValueManager.cs
+++ alien-interrogation-game/Assets/Scripts/GameValueManager.cs
@@ -6,11 +6,20 @@ public class GameValueManager : MonoBehaviour
 {
     public static GameValueManager instance;
 
+    //used to track if one point of contradiction has been seen
     bool point1 = false;
+    //used to track if another point of contradiction has been seen
     bool point2 = false;
+    //used to see if currently at a point of contradiction
     bool atPoint = false;
+    //used to record important pieces of info seen
+    List<string> convoKnowledge = new List<string>();
+    //used to track if important info has been gathered from each character
+    List<bool> acuKnowledge = new List<bool>();
 
 
+    //checks if both points of contradiction have been seen and if the player is currently at a point of contradiction 
+    //if true, it means the player can successfully confront the character they are talking to
     bool canConfront()
     {
         if (instance.point1 == true && instance.point2 == true && instance.atPoint == true)
@@ -38,4 +47,35 @@ public class GameValueManager : MonoBehaviour
     {
         instance.atPoint = val;
     }
+
+
+    void addConvoKnowledge(string info)
+    {
+        instance.convoKnowledge.Add(info);
+    }
+
+
+    string getConvoKnowledgeAt(int index)
+    {
+        return instance.convoKnowledge[index];
+    }
+
+
+    void clearConvoKowledge()
+    {
+        instance.convoKnowledge.Clear();
+    }
+
+
+    void addAcuKnowledge(bool info)
+    {
+        instance.acuKnowledge.Add(info);
+    }
+
+
+    bool getAcuKnowledgeAt(int index)
+    {
+        return instance.acuKnowledge[index];
+    }
+
 }
